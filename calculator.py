1573e2beb1098b9d7e8cc973b33bef3c8066a811
1Mr-Newton <eaidoo015@gmail.com>
2022-03-25 08:28:36 +0000

    commit

--- calculator.py
+++ calculator.py
@@ -1,7 +1,31 @@
 print("Welcome to the Newton's Number bases convertor")
 print()
 # import readch
+def fromHex2B10(hex_number):
+  new_num= []
+  hex_alpha = 'ABCDEFG'
+  hex_nums = ['10','11','12','13','14','15', '16']
+  for num in str(hex_number):
+    if num in hex_alpha:
+      num = hex_nums[int(hex_alpha.index(str(num)))]
+  if int(num) >= 16:
+    return (f"This is not a base {16} number!")
+  else:
+    answer1 = 0
+    for l in hex_number:
+      if l in hex_alpha:
+        k = int(hex_nums[int(hex_alpha.index(str(l)))])
+        new_num.append(k)
+      else:
+        new_num.append(l)
 
+    new_num.reverse() #
+    for jj in new_num:
+      answer1 += int(jj) * (16**(int(new_num.index(jj))))
+    return answer1
+
+
+    
 def base10toANY(base, number):
   answer = ""
   while number > 0:
--- test.py
+++ test.py
@@ -1,73 +1,28 @@
-#  This is Newton
-# In this video I'm going to show you how to you how to write an advanced python number bases convertor using no third party libraries.
-# # Let's start...
-
-# print("Welcome to Newton's Number Bases convertor")
-
-# # Let's write some functions to take the number and convert is to base 10
-
-# def convert2base10(number, current_num_base):
-#   '''
-#   This function takes any number from any base and convert it to base 10
-#   # '''
-
-#   # ind = -1
-#   # for n in str(number):
-
-
-
-def calculator(base, number):
-  answer = ""
-  # number = 20
-  while number > 0:
-    k = number % base
-    number = number // base
-    answer += str(k)
-  print(answer[::-1])
-
-
-hex_alpha = 'ABCDEF'
-hex_nums = ['10','11','12','13','14','15']
-# answer = ""
-# number  = 10861007
-# while number > 0:
-#   k = number % 16
-#   number = number // 16
-#   if str(k) in hex_nums:
-#     k = hex_alpha[int(hex_nums.index(str(k)))]
-#   answer += str(k)
-# print(answer[::-1])
-
-s = 'F5'
-for l in s:
-  if l in hex_alpha:
-    k = int(hex_nums[int(hex_alpha.index(str(l)))])
-
-print(k)
-
-def fromHex2B10(number):
-  hex_alpha = 'ABCDEF'
-  hex_nums = ['10','11','12','13','14','15']
-  for f in str(number):
-    pass
-  if int(f) >= 16:
-    print(f"This is not a base {16} number!")
-    print()
-    return
+def fromHex2B10(hex_number):
+  new_num= []
+  hex_alpha = 'ABCDEFG'
+  hex_nums = ['10','11','12','13','14','15', '16']
+  for num in str(hex_number):
+    if num in hex_alpha:
+      num = hex_nums[int(hex_alpha.index(str(num)))]
+  if int(num) >= 16:
+    return (f"This is not a base {16} number!")
   else:
-    ind =- 1
     answer1 = 0
-    
-    for l in s:
+    for l in hex_number:
       if l in hex_alpha:
         k = int(hex_nums[int(hex_alpha.index(str(l)))])
-        number = (k*16)
+        new_num.append(k)
+      else:
+        new_num.append(l)
 
-    for n in str(number)[::-1]:
-      ind += 1
-      answer1 += int(n) * (16**int(ind))
-
+    new_num.reverse() #
+    for jj in new_num:
+      answer1 += int(jj) * (2**(int(new_num.index(jj))))
     return answer1
 
 aa = fromHex2B10('A5')
 print(aa)
+
+
+# a ='1D7F'
