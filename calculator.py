f7b95a384356d64195efc8fe2f03f9124e7db9ac
1Mr-Newton <eaidoo015@gmail.com>
2022-03-25 10:22:32 +0000

    commit

--- calculator.py
+++ calculator.py
@@ -1,6 +1,3 @@
-print("Welcome to the Newton's Number bases convertor")
-print()
-# import readch
 def fromHex2B10(hex_number):
   new_num= []
   hex_alpha = 'ABCDEFG'
@@ -46,60 +43,66 @@ def base10toANY(base, number):
     number = number // base
     answer += str(k)
   return answer[::-1]
-  # print(answer[::-1])
-
-# base10toANY(2, 15)
-
 
 def toBase10(from_base, number):
   for f in str(number):
     pass
-  if int(f) >= from_base:
-    print(f"This is not a base {from_base} number!")
-    print()
-    return
-  ind =- 1
-  answer1 = 0
-  for n in str(number)[::-1]:
-    ind += 1
-    answer1 += int(n) * (from_base**int(ind))
-
-  return answer1
-  # print(answer1)
-
-# toBase10(2, 1111)
-
-calculating = True
-while calculating:
-  # print(fromHex2B10('A5'))
-  user_choice = input("Please choose one of the following services:\n1. From any base to any base(eg. from base 8 to base 2)\n2. From any base to base 10(eg. from base 8 to base 10)\nEnter your choice here: ")
-  print()
-
-  if user_choice == '1':
-    from_base = int(input('Please enter the "from" base(eg. from base 2)\n'))
-    number = int(input('Please enter the number you want to convert\n'))
-    conv2base10 = toBase10(from_base, number)
-    to_base = int(input('Please enter the "to" base(eg. to base 8)\n'))
-    final_ans = base10toANY(to_base, conv2base10)
-    print(final_ans)
-
-
-  elif user_choice == '2':
-    from_base = int(input('Please enter the "from" base(eg. from base 2)\n'))
-    
-    number = input('Please enter the number you want to convert\n')
-
-    if from_base == 16:
-      print(fromHex2B10(number))
-
+  try:
+    if int(f) >= from_base:
+      return f"This is not a base {from_base} number!"
+  
     else:
-      final_an = toBase10(from_base, number)
-      print(final_an)
+      ind =- 1
+      answer1 = 0
+      for n in str(number)[::-1]:
+        ind += 1
+        answer1 += int(n) * (from_base**int(ind))
+      return int(answer1)
+  except:
+    print('Check the base you chose')
 
+
+def main():
+  print("Welcome to the Newton's Number bases convertor")
   print()
+  calculating = True
+  while calculating:
+    user_choice = input("Please choose one of the following services:\n1. From any base to any base(eg. from base 8 to base 2)\n2. From any base to base 10(eg. from base 8 to base 10)\nEnter your choice here: ")
+    print()
 
-  calculate_again = input("Do you have more to calculate?\nPress 'y' to continue or anykey to quit:")
-  if calculate_again == 'y':
-    continue
-  else:
-    break
+    if user_choice == '1':
+      from_base = int(input('Please enter the "from" base(eg. from base 2)\n'))
+      number = input('Please enter the number you want to convert\n')
+
+      to_base = int(input('Please enter the "to" base(eg. to base 8)\n'))
+
+      if from_base == 16:
+        print('The answer is', base10toANY(to_base, fromHex2B10(number)))
+      else:
+        try:
+          print('The answer is', base10toANY(to_base, toBase10(from_base, number)))
+        except:
+          pass
+          # print('Check your chosen base and number')
+
+
+    elif user_choice == '2':
+      from_base = int(input('Please enter the "from" base(eg. from base 2)\n'))
+      
+      number = input('Please enter the number you want to convert\n')
+
+      if from_base == 16:
+        print('The answer is', fromHex2B10(number))
+
+      else:
+        final_an = toBase10(from_base, number)
+        print('The answer is', final_an)
+
+    calculate_again = input("Do you have more to calculate?\nPress 'y' to continue or anykey to quit:")
+    if calculate_again == 'y':
+      continue
+    else:
+      break
+
+if __name__ == "__main__":
+  main()
